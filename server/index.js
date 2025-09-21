2719550b42f770742aa8178b90ba06edf710d10e
Raju Bhgagat <bhagat.raju100@example.com>
2025-09-22 02:25:29 +0530

    Fix change frontendurl in backend

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://studynotion-frontend-eight.vercel.app/",
 		credentials:true,
 	})
 )
