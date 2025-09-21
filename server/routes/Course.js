e633db526c89ff4dcbcf6fb7f40dca150c6c53bf
Raju Bhgagat <bhagat.raju100@example.com>
2025-09-22 02:00:29 +0530

    Fix RatingAndReview  typo

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -43,7 +43,7 @@ const {
   createRating,
   getAverageRating,
   getAllRating,
-} = require("../controllers/RatingandReview")
+} = require("../controllers/RatingAndReview")
 const {
   updateCourseProgress,
   getProgressPercentage,
